c026a59adc467253ddf57cdabb8e79d254060e45
berenddeneef <berenddeneef@gmail.com>
2023-01-03 15:52:41 +0100

    Initial commit

---
+++ src/main/java/net/berend05be/freezingmod/FreezingMod.java
@@ -0,0 +1,14 @@
+package net.berend05be.freezingmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FreezingMod implements ModInitializer {
+	public static final String MOD_ID = "freezingmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/berend05be/freezingmod/FreezingModClient.java
@@ -0,0 +1,10 @@
+package net.berend05be.freezingmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FreezingModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
