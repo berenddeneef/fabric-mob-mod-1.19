5c1eadbd82ce0e46f46d2507dad4da1c478dcb09
berenddeneef <berenddeneef@gmail.com>
2023-01-03 16:17:38 +0100

    added important comment

--- src/main/java/net/berend05be/freezingmod/FreezingMod.java
+++ src/main/java/net/berend05be/freezingmod/FreezingMod.java
@@ -3,7 +3,7 @@ package net.berend05be.freezingmod;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+//Very important comment
 public class FreezingMod implements ModInitializer {
 	public static final String MOD_ID = "freezingmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
